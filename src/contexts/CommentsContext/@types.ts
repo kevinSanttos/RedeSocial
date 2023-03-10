f7098f2129f4b7f0ea843e99452e08a825022c2b
Djeff <djeff.walla99@gmail.com>
2023-03-10 12:19:21 -0300

    feat: followerContext e CommntsContext

--- src/contexts/CommentsContext/@types.ts
+++ src/contexts/CommentsContext/@types.ts
@@ -1,3 +1,16 @@
 export interface IDefaultProviderProps {
   children: React.ReactNode;
 }
+
+export interface ICommentsContext{
+  createComments: (formData: IComments) => Promise<void>,
+  deleteComments: (idComments: number) => Promise<void>,
+  updateComments: (formData: IComments, idComments: number) => Promise<void>,
+  comments: IComments[] | null
+}
+export interface IComments {
+  userId: number,
+  postId: number,
+  description: string,
+  id: number
+}
