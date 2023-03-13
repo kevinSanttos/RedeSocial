ec38e6de4dd10b3188f8f6801f9f3851e101cf83
kevin <kevinkevinbarbosa@gmail.com>
2023-03-13 14:22:44 -0300

    featDesigne-UserPage_

--- src/components/Posts/index.tsx
+++ src/components/Posts/index.tsx
@@ -1,35 +1,9 @@
-import { AvatarFollower } from "../AvatarFollower";
-<<<<<<< HEAD
-import perfil from "../../assets/perfil.jpg"
-import SemCurti from "../../assets/SemCurti.png"
-import {StyledLisFollower, StyledDivFooterPost} from "./style"
-
-export const Posts = () => {
-    return (
-        <StyledLisFollower>
-            <li>
-                <AvatarFollower fotoAvatar={perfil} nome={"Percival"} level={"front"}/>
-                <h2>Inteligencia Artificial</h2>
-                <span>
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda 
-                    repudiandae ipsa culpa similique consequuntur ab ad amet? In voluptates 
-                    fugiat sed, mollitia dolor aut aliquam iusto, placeat molestiae, ut alias?
-                </span>
-                <StyledDivFooterPost>
-                    <button>Abrir Post</button>
-                    <div>
-                        <img src={SemCurti} alt="SemCurti" />
-                        <span>0</span>
-                    </div>
-                </StyledDivFooterPost>
-            </li>
-        </StyledLisFollower>
-=======
 import SemCurti from "../../assets/SemCurti.png"
 import {StyledLisFollower, StyledDivFooterPost} from "./style"
 import { useContext } from "react";
 import { PostsContext } from "../../contexts/PostsContext";
 import { UserContext } from "../../contexts/UserContext";
+import { AvatarFollower } from "../AvatarFollower";
 
 export const Posts = () => {
     const {posts} = useContext(PostsContext)
@@ -71,8 +45,7 @@ export const Posts = () => {
                     ) : (
                         <div>Sem posts</div>
                     )
-             }    
-        </>
->>>>>>> 92cbc094993054b318309bfdd5ff95104266b467
-    );
-  };
+                    }
+                     </>
+         )     
+                }
